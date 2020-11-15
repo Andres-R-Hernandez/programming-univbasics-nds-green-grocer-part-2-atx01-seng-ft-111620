f43396ce4514623002420e8ceb4fedd92482bf9c
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 17:23:55 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,28 +4,6 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-
-
-=begin
-  coupon_items = []
-  coupons.each do |coupon_item|
-    cart.each do |cart_item|
-      if cart_item[:name] == coupon_item[:name]
-        coupon_items <<
-        {
-        :item => cart_item[:item] + " W/COUPON",
-        :price => coupon_item[:cost] / coupon_item[:num],
-        :clearance => cart_item[:clearance],
-        :count => cart_item[:count] - (cart_item[:count] % coupon_item[:num]),
-        }
-        cart_item[:count] = cart_item[:count] % coupon_item[:num]
-      end
-    end
-  end
-=end
-
-
-#=begin
   coupon_items = cart.each_with_object([]) do |cart_item,temp|
     coupons.each do |coupon_item|
       if cart_item[:item] == coupon_item[:item]
@@ -43,33 +21,6 @@ def apply_coupons(cart, coupons)
     cart << item
   end
   cart
-#=end
-
-=begin
-  new_cart = cart.each_with_object([]) do |cart_item,temp|
-    coupons.each do |coupon_item|
-      if cart_item[:item] == coupon_item[:item]
-        new_cart_item = {
-        :item => cart_item[:item] + " W/COUPON",
-        :price => coupon_item[:cost] / coupon_item[:num],
-        :clearance => cart_item[:clearance],
-        :count => cart_item[:count] - (cart_item[:count] % coupon_item[:num]),
-        }
-
-        cart_item[:count] = cart_item[:count] % coupon_item[:num]
-
-        temp << cart_item
-        temp << new_cart_item
-      else
-        temp << cart_item
-      end
-    end
-  end
-
-  p new_cart
-=end
-
-
 end
 
 def apply_clearance(cart)
