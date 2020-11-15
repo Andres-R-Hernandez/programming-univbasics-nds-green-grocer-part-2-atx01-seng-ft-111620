14d53b81b78ad4029742ff5d39b103600fcdd454
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 16:22:59 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,11 +4,9 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-
   cart.each do |cart_item|
     coupons.each do |coupon_item|
       if cart_item[:name] == coupon_item[:name]
-
         new_cart_item =
         {
         :item => cart_item[:item] + " W/COUPON",
@@ -16,11 +14,8 @@ def apply_coupons(cart, coupons)
         :clearance => cart_item[:clearance],
         :count => cart_item[:count] - (cart_item[:count] % coupon_item[:num]),
         }
-
         cart_item[:count] = cart_item[:count] % coupon_item[:num]
-
         cart << new_cart_item
-
       end
     end
   end
